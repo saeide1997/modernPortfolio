109387c09f948331f875a9285e5fa738fafd9cba
ANIRUDDHA ADAK <aniruddhaadak80@gmail.com>
2024-11-15 21:21:16 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { HeroSection } from "@/components/sections/hero"
 import { AboutSection } from "@/components/sections/about"
 import { ProjectSection } from "@/components/sections/project"
 import { SkillSection } from "@/components/sections/skills"
-import { ExperienceSection } from "@/components/sections/experience"
+import { ExperienceSection } from "@/components/sections/experiences"
 
 
 
