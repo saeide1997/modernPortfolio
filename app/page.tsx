c8aa4c43b4bbaf54109fa67ae8492983e2142ddd
ANIRUDDHA ADAK <aniruddhaadak80@gmail.com>
2024-11-15 21:15:47 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,18 @@ import { HeroSection } from "@/components/sections/hero"
 import { AboutSection } from "@/components/sections/about"
 import { ProjectSection } from "@/components/sections/project"
 import { SkillSection } from "@/components/sections/skills"
-
+import { ExperienceSection } from "@/components/sections/experiences"
 
 
 
 export default function Home() {
   return (
-    <>
     <div className="flex flex-col">
       <HeroSection />
       <AboutSection />
       <ProjectSection/>
       <SkillSection/>
+      <ExperienceSection/>
     </div>
-    </>
   )
 }
